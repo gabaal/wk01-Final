6ec6a14f1b636cf49e728cdfae03f1324ec2cff8
gabaal <geraldbaalham@live.co.uk>
2024-01-14 15:26:17 +0000

    Read More complete for featured and tours sections

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 console.log("app.js linked and working");
+
+//title and nav hide on scroll
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
@@ -9,3 +11,39 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+//featured section read more show / hide
+let featureText = document.getElementById("featureReadMoreText");
+featureText.style.display = `none`;
+let featureIsShow = false;
+
+const readMore = document.getElementById("featureReadMore");
+readMore.addEventListener("click", featureToggleText);
+
+function featureToggleText() {
+  if (featureIsShow) {
+    featureText.style.display = `none`;
+    featureIsShow = false;
+  } else {
+    featureText.style.display = `block`;
+    featureIsShow = true;
+  }
+}
+
+//tours section read more show / hide
+let toursText = document.getElementById("toursreadMoreText");
+toursText.style.display = `none`;
+let toursIsShow = false;
+
+const toursReadMore = document.getElementById("toursReadMore");
+toursReadMore.addEventListener("click", toursToggleText);
+
+function toursToggleText() {
+  if (toursIsShow) {
+    toursText.style.display = `none`;
+    toursIsShow = false;
+  } else {
+    toursText.style.display = `block`;
+    toursIsShow = true;
+  }
+}
